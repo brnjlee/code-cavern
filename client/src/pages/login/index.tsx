b8f1936d990a35706c97aca68f2e1a96bf93c147
Brian Lee <brnjlee@gmail.com>
2023-03-20 22:58:56 -0400

    configure next-auth

---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Login from "../../components/Login";
+
+export default () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-slate-100">
+      <Login />
+    </div>
+  );
+};
